7c742e7fa37203cc8eea0240ec31bc90a3c869d8
PScud <escudie.pierre@gmail.com>
2024-02-28 10:31:16 -0500

    UserService tested at 96 percent

--- src/test/java/com/oc/paymybuddy/UserServiceTest.java
+++ src/test/java/com/oc/paymybuddy/UserServiceTest.java
@@ -4,10 +4,12 @@ import com.oc.paymybuddy.model.User;
 import com.oc.paymybuddy.repository.UserRepository;
 import com.oc.paymybuddy.service.MockDBService;
 import com.oc.paymybuddy.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpSession;
 
 import java.util.Optional;
 
@@ -19,7 +21,7 @@ import static org.mockito.Mockito.*;
 public class UserServiceTest {
 
     @Autowired
-    public UserService userservice;
+    public UserService userService;
 
     @MockBean
     public UserRepository userRepository;
@@ -41,7 +43,7 @@ public class UserServiceTest {
         bobRoss.setEmail(email);
         bobRoss.setPassword(password);
 
-        userservice.saveUserWithBasicInfo(firstName, lastName, email, password);
+        userService.saveUserWithBasicInfo(firstName, lastName, email, password);
 
         verify(userRepository, times(1)).save(bobRoss);
     }
@@ -62,7 +64,7 @@ public class UserServiceTest {
 
         when(userRepository.findByEmail(email)).thenReturn(bobRoss);
 
-        User result = userservice.findByEmail(email);
+        User result = userService.findByEmail(email);
 
         assertEquals(result, bobRoss);
     }
@@ -94,9 +96,106 @@ public class UserServiceTest {
 
         when(userRepository.findById(anyLong())).thenReturn(Optional.of(bobRoss));
 
-        userservice.modifyUserInfo(modifiedBobRoss);
+        userService.modifyUserInfo(modifiedBobRoss);
 
         verify(userRepository, times(1)).saveAndFlush(bobRoss);
+
+    }
+
+    @Test
+    public void buyCredit() {
+
+        String firstName = "Bob";
+        String lastName = "Ross";
+        String email = "bobross@gmail.com";
+        String password = "12345";
+
+        User bobRoss = new User();
+        bobRoss.setFirstName(firstName);
+        bobRoss.setLastName(lastName);
+        bobRoss.setEmail(email);
+        bobRoss.setPassword(password);
+        bobRoss.setBalance(0);
+
+        double purchasedCredit = 1000.50;
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(bobRoss));
+
+        userService.buyCredit(bobRoss, purchasedCredit);
+
+        verify(userRepository, times(1)).save(bobRoss);
+
+        assertEquals(1000.50, bobRoss.getBalance());
+    }
+
+    @Test
+    public void setSenderBalance() {
+
+        double initialBalance = 1000.0;
+        double amount = 100.0;
+        double newBalance = initialBalance - amount;
+
+        User sender = new User();
+        sender.setBalance(initialBalance);
+
+        HttpSession session = new MockHttpSession();
+        session.setAttribute("user", sender);
+
+        userService.setSenderBalance(session, amount);
+
+        session.setAttribute("balance", newBalance);
+
+        assertEquals(newBalance, sender.getBalance(), 0.0);
+        verify(userRepository, times(1)).save(sender);
+        assertEquals(newBalance, session.getAttribute("balance"));
+    }
+
+    @Test
+    public void setReceiverBalance() {
+
+        double initialBalance = 1000.0;
+        double amount = 100.0;
+        double newBalance = 1100.0;
+
+        User receiver = new User();
+        receiver.setBalance(initialBalance);
+
+        userService.setReceiverBalance(receiver, amount);
+
+        verify(userRepository, times(1)).save(receiver);
+        assertEquals(newBalance, receiver.getBalance(), 0.0);
+    }
+
+    @Test
+    public void hasSufficientBalance() {
+
+        double amount = 500.0;
+
+        User bobRoss = new User();
+        bobRoss.setBalance(1000.0);
+
+        HttpSession session = new MockHttpSession();
+        session.setAttribute("user", bobRoss);
+
+        boolean result = userService.hasSufficientBalance(session, amount);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void findById() {
+
+        long bobId = 1;
+
+        User bobRoss = new User();
+        bobRoss.setUserId(bobId);
+
+        when(userRepository.findById(bobId)).thenReturn(Optional.of(bobRoss));
+
+        User result = userService.findById(bobId);
+
+        verify(userRepository, times(1)).findById(bobId);
+        assertEquals(result, bobRoss);
 
     }
 
